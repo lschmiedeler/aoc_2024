7003800a7f2b2fde0ed5f5418ac8f30456947001
Lauren Schmiedeler <leschmiedeler@gmail.com>
2024-12-02 18:24:03 -0600

    advent of code 2024 day 2

---
+++ Day2/Day2.R
@@ -0,0 +1,26 @@
+puzzle <- 2
+
+connection <- file(description = "Day2Input.txt", open = "r")
+reports <- readLines(connection)
+close(connection)
+
+check_if_report_safe <- function(report) {
+  diff_report <- diff(report)
+  return(sum((diff_report >= 1 & diff_report <= 3)) == length(diff_report) |  sum(diff_report >= -3 & diff_report <= -1) == length(diff_report))
+}
+
+sum(sapply(reports, function(report) {
+  report <- as.integer(strsplit(report, "\\s+")[[1]])
+  safe <- check_if_report_safe(report)
+  if (puzzle == 2) { 
+    i <- 1
+    while (i <= length(report)) {
+      if (safe) { break }
+      else {
+        safe <- check_if_report_safe(report[-c(i)])
+        i <- i + 1
+      }
+    }
+  }
+  safe
+}))
